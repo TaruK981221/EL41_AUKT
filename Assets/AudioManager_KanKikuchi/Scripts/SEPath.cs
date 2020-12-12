2b968a1364d8de04d5651137db9c924cb1605aee
TaruK981221 <tarui2412git@gmail.com>
2020-12-12 10:59:10 +0900

    いろんな音追加

--- Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
+++ Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -5,9 +5,19 @@
 /// </summary>
 public static class SEPath{
 
-	public const string JINGLE10 = "SE/Jingle/jingle10";
-	public const string SYSTEM20 = "SE/system20";
-	public const string SYSTEM23 = "SE/system23";
+	public const string CANCEL2                  = "SE/cancel2";
+	public const string COIN03                   = "SE/coin03";
+	public const string DECISION                 = "SE/Decision";
+	public const string DROP                     = "SE/drop";
+	public const string HAGIKU                   = "SE/Hagiku";
+	public const string IN                       = "SE/in";
+	public const string JINGLE10                 = "SE/Jingle/jingle10";
+	public const string KICK                     = "SE/kick";
+	public const string RIZALT                   = "SE/rizalt";
+	public const string SE_MAOUDAMASHII_BATTLE09 = "SE/se_maoudamashii_battle09";
+	public const string SE_MAOUDAMASHII_SYSTEM37 = "SE/se_maoudamashii_system37";
+	public const string SYSTEM20                 = "SE/system20";
+	public const string SYSTEM23                 = "SE/system23";
 
 }
 
