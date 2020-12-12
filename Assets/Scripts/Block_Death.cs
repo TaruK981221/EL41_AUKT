f1f855974627b895e247695098176d7421aff1cd
TaruK981221 <tarui2412git@gmail.com>
2020-12-12 12:39:16 +0900

    BGM追加 プレイヤーの動き追加 壁のプレハブ追加

--- Assets/Scripts/Block_Death.cs
+++ Assets/Scripts/Block_Death.cs
@@ -16,8 +16,11 @@ public class Block_Death : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "Ball")
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
