5172d8536a1d2d6810705445b2ccf7842241eb38
pusanmodoki <blackdragon.chanp@gmail.com>
2020-12-12 13:07:05 +0900

    Hidekiがいっぱい増えた

--- Assets/Scripts/Ball/ExplosionBall.cs
+++ Assets/Scripts/Ball/ExplosionBall.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExplosionBall : MonoBehaviour
+public class ExplosionBall : BaseBall
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	GameObject m_explosionEffect = null;
+	[SerializeField]
+	float m_explosionTimeRangeStart = 5.0f;
+	[SerializeField]
+	float m_explosionTimeRangeEnd = 10.0f;
 
-    // Update is called once per frame
-    void Update()
+	float m_counter = 0.0f;
+
+	protected override void Start()
+	{
+		base.Start();
+		m_counter = Random.Range(m_explosionTimeRangeStart, m_explosionTimeRangeEnd);
+	}
+
+	// Update is called once per frame
+	void Update()
     {
-        
-    }
+		m_counter -= Time.deltaTime;
+
+		if (m_counter <= 0.0f)
+		{
+			var obj = Instantiate(m_explosionEffect);
+			obj.transform.position = transform.position;
+			obj.SetActive(true);
+			Destroy(gameObject);
+		}
+	}
 }
---
+++ Assets/Scripts/DestoroyWhenParticleEnd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoroyWhenParticleEnd : MonoBehaviour
+{
+	ParticleSystem[] m_particles = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+		m_particles = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		foreach (var particle in m_particles)
+			if (particle.isPlaying) return;
+
+		Destroy(gameObject);
+    }
+}
